9e76aa788f0e0712f78a3d1524a5e0c353bb4a06
Mechail <sephrone_wow@live.com>
2021-10-13 16:25:54 +1100

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -15,7 +15,7 @@
 *   (MW - 12/10/21)
 *   A lot of good work here, but also a number of issues that I just don't have time to work on with the time I have left today (unfortunatly I need sleep)
 *   I will make a bigger effort on adding code tomorrow, just needed to take time today to make sure the fondations are correct and everything is set up correctly so there wont be issues.
-*       -> Fpr example, there are a number of things that will need to be re-written to work as expected
+*       -> For example, there are a number of things that will need to be re-written to work as expected
 *
 *   IN REGARDS TO THE DATABASES:
 *       - It says we need a primary DB, but I think 3 seperate DBs makes more sense
@@ -38,7 +38,8 @@
 #include <iostream> 
 #include <fstream> 
 #include <string>
-#include <ctime> // Required for date/time format - MW
+#include <vector> // - MW
+#include <sstream> // - MW
         
 using namespace std; 
 
@@ -48,19 +49,18 @@ using namespace std;
 * - Might need some pointers for current user, etc as globals so I'll keep things posted.
 * 
 */
-int patientID;
-string fname;
-string lname;
-int doB;
-string address;
-string visitedLocation;
-int date;
-string overseasTravel;
-string covidTest;
-string status;
-
-fstream myfile; 
-
+//int patientID;
+//string fname;
+//string lname;
+//int doB;
+//string address;
+//string visitedLocation;
+//int date;
+//string overseasTravel;
+//string covidTest;
+//string status;
+//
+//fstream myfile; 
 
 
 /**
@@ -78,29 +78,89 @@ fstream myfile;
 * - The formatting of the text files should be noted though (good for report!) as it is how they will be read in and out
 * 
 */
-void CreateDatabases()
+//void CreateDatabases()
+//{
+//    myfile.open("patient_detail_database.txt"); 
+//    myfile << "Patient Detail Table\n\n"; 
+//    myfile <<"Field Name\tData Type\tDescription\nPatient ID\tInteger\nName\tVar Char\tName of the patient\nDate of birth\tInteger\nAddress\tVar Char\nVisited Location\tVar Char\nDate/Timing\tDateTime\nLast overseas travel\tString\tYes/No\nCovid Test\tString\tPositive/Negative\nStatus\tString\tAlive/Dead" <<endl <<endl; 
+//    myfile.close();
+//
+//    myfile.open("symptom_database.txt");  
+//    myfile << "Symptom Table\n\n";
+//    myfile << "Field Name\tData Type\tDescription\nLow Risk\tVar Char\t Fever, Dry Cough\nMedium Risk\tVar Char\nHigh Risk\tVar char" << endl <<endl;
+//    myfile.close();
+//
+//    myfile.open("location_database.txt");
+//    myfile << "High Risk Covid Location\n\n";
+//    myfile << "Field Name\tData Type\tDescription\nid\tVar Char\tAuburn Train Station\nid\tVar Char\tRoyal Hotel";
+//    myfile.close();
+//   
+//}
+
+
+
+/**
+* OPTION 5: Prints out the current users info as it is currently stored in the DB
+* @param ...
+* @returns ...
+*
+* - If the Patient detail is empty you should display "[] - the database is empty"
+*
+* (MW - 12/10/21)
+* - This needs to get the details of THE CURRENT PATIENT (not the whole DB)
+* - Just scan through patient DB and print out details at line of patient ID
+*
+*/
+void DisplayPositivePatients()
 {
-    myfile.open("patient_detail_database.txt"); 
-    myfile << "Patient Detail Table\n\n"; 
-    myfile <<"Field Name\tData Type\tDescription\nPatient ID\tInteger\nName\tVar Char\tName of the patient\nDate of birth\tInteger\nAddress\tVar Char\nVisited Location\tVar Char\nDate/Timing\tDateTime\nLast overseas travel\tString\tYes/No\nCovid Test\tString\tPositive/Negative\nStatus\tString\tAlive/Dead" <<endl <<endl; 
-    myfile.close();
-
-    myfile.open("symptom_database.txt");  
-    myfile << "Symptom Table\n\n";
-    myfile << "Field Name\tData Type\tDescription\nLow Risk\tVar Char\t Fever, Dry Cough\nMedium Risk\tVar Char\nHigh Risk\tVar char" << endl <<endl;
-    myfile.close();
-
-    myfile.open("location_database.txt");
-    myfile << "High Risk Covid Location\n\n";
-    myfile << "Field Name\tData Type\tDescription\nid\tVar Char\tAuburn Train Station\nid\tVar Char\tRoyal Hotel";
-    myfile.close();
-   
+    //string line;
+    //myfile.open("patient_detail_database.txt");
+    //if (myfile.is_open())
+    //{
+    //    while (getline(myfile, line))
+    //    {
+    //        cout << line << '\n'; // this also needs to be changed to display "[]" if db is empty
+    //    }
+    //}
+    //myfile.close();
 }
 
 
 
 /**
-* Prints out a list of high risk locations to the terminal (for user to read)
+* OPTION 4: Allows the current user to update their details and saves them to the DB
+* @param ...
+* @returns ...
+*
+* Remember:
+* - Check for illigal characters
+*       -> "Unknown selection, please try again"
+* - If the Patient detail is empty you should display "[] - the database is empty"
+* - Alow for upper/lower characters
+*
+* (MW - 12/10/21)
+* - CURRENT PATIENT
+* - Find line in DB using patient ID
+* - Get new variables
+* - Update via overwrting info at that line in the DB
+*/
+void UpdatePatientDetails()
+{
+    ////get by patientID or name
+    //cout << "Enter your address:";
+    //cin >> address;
+    //cout << "Have you travelled overseas in the last 6 months, Y/N?";
+    //cin >> overseasTravel;
+    //cout << "Please select your visited location from the High Risk COVID areas:"; // similar to above, possibly display the db of high risk locations and display options 1-10 
+    //cin >> visitedLocation;
+    //cout << "Status:" << "1.Cured" << endl << "2.Dead";
+    //cin >> status; // to be fixed
+}
+
+
+
+/**
+* OPTION 3: Prints out a list of high risk locations to the terminal (for user to read)
 * @param ...
 * @returns ...
 * 
@@ -111,132 +171,22 @@ void CreateDatabases()
 */
 void DisplayUpdatedLocation()
 {
-    string line;
-    myfile.open("location_database.txt");
-    if (myfile.is_open()) 
-    { 
-        while ( getline(myfile, line) ) 
-        { 
-        cout << line << '\n'; 
-        }
-    } 
-    myfile.close();
+    //string line;
+    //myfile.open("location_database.txt");
+    //if (myfile.is_open()) 
+    //{ 
+    //    while ( getline(myfile, line) ) 
+    //    { 
+    //    cout << line << '\n'; 
+    //    }
+    //} 
+    //myfile.close();
 }
 
 
 
 /**
-* Allows the current user to update their details and saves them to the DB
-* @param ...
-* @returns ...
-* 
-* Remember:
-* - Check for illigal characters
-*       -> "Unknown selection, please try again"
-* - If the Patient detail is empty you should display "[] - the database is empty"
-* - Alow for upper/lower characters
-* 
-* (MW - 12/10/21)
-* - CURRENT PATIENT
-* - Find line in DB using patient ID
-* - Get new variables
-* - Update via overwrting info at that line in the DB
-*/
-void UpdatePatientDetails()
-{
-    //get by patientID or name
-    cout << "Enter your address:";
-    cin >> address;
-    cout << "Have you travelled overseas in the last 6 months, Y/N?";
-    cin >> overseasTravel;
-    cout << "Please select your visited location from the High Risk COVID areas:"; // similar to above, possibly display the db of high risk locations and display options 1-10 
-    cin >> visitedLocation;
-    cout << "Status:" <<"1.Cured" << endl << "2.Dead";
-    cin >> status; // to be fixed
-}
-
-
-
-/**
-* Prints out the current users info as it is currently stored in the DB
-* @param ...
-* @returns ...
-* 
-* - If the Patient detail is empty you should display "[] - the database is empty"
-* 
-* (MW - 12/10/21)
-* - This needs to get the details of THE CURRENT PATIENT (not the whole DB)
-* - Just scan through patient DB and print out details at line of patient ID
-* 
-*/
-void DisplayPositivePatients()
-{
-    string line;
-    myfile.open("patient_detail_database.txt");
-    if (myfile.is_open()) 
-    {
-         while ( getline(myfile, line) ) 
-        { 
-        cout << line << '\n'; // this also needs to be changed to display "[]" if db is empty
-        }
-    }
-    myfile.close();
-}
-
-
-
-/**
-* Takes in new user info, stores info to DB, and also recommends if user needs a test
-* @param ...
-* @returns ...
-*
-* Remember:
-* - Check for illigal characters
-*       -> "Unknown selection, please try again"
-* - If the Patient detail is empty you should display "[] - the database is empty"
-* - Alow for upper/lower characters
-* 
-* (MW - 12/10/21)
-* - All DBs required
-* - For patient ID: scan through DB line by line only checking first column (ID)
-*       -> No match proceed / match "This ID exists"
-*       -> Order is not an issue atm
-* - If at High Risk Location with any symptoms, recomend test
-* - If High Risk Symptoms, recomend test
-* - Other symptoms -> "isolate at home" or something like that
-*       -> At least Try 5 more combination from real life scenario (???)
-* - If the Symptom database is empty you should display, "Unable to recommend COVID Test – required data missing".
-* 
-*/
-void CovidTestRecommendationDetails()
-{
-    
-    cout << "Patient Details" << endl;
-    cout << "Enter your patient ID:";
-    cin >> patientID; // if patientID already exists then cant send to db
-    cout << "Enter your First name:";
-    cin >> fname;
-    cout << "Enter your Family name:";
-    cin >> lname;
-    cout << "Enter your Date of birth:";
-    cin >> doB;
-    cout << "Enter your address:";
-    cin >> address;
-    cout << "Have you travelled overseas in the last 6 months, Y/N?";
-    cin >> overseasTravel;
-    cout << "Please select your symptoms:"; // could just make an if/switch statement for this one with options 1-3. eg. 1: (Low risk) Dry cough. 2: (Medium risk) Fever
-    //cin >> 
-    cout << "Please select your visited location from the High Risk COVID areas:"; // similar to above, possibly display the db of high risk locations and display options 1-10 
-    cin >> visitedLocation;
-
-    //send variables to db
-
-}
-
-
-
-/**
-* Takes in new user info, stores info to DB, and also recommends if user needs a test
+* OPTION 2: Takes in new user info, stores info to DB, and also recommends if user needs a test
 * @param ...
 * @returns ...
 *
@@ -256,29 +206,128 @@ void CovidTestRecommendationDetails()
 */
 void UpdateTestStatus()
 {
-    int var;
-    cout << "Enter your PatientID:";
-    cin >> patientID;
-    cout << "Enter your covid test status:";
-    cin >> status;
-    cout << "Enter your visited location:";
-    cin >> visitedLocation;
-    cout << "Enter 1 for positive.\nEnter 2 for Negative.";
-    cin >> var;
-    if(var == 1)
-    {
-        //update this patient is positive in the db
-        //update location database of new high risk location
+    //int var;
+    //cout << "Enter your PatientID:";
+    //cin >> patientID;
+    //cout << "Enter your covid test status:";
+    //cin >> status;
+    //cout << "Enter your visited location:";
+    //cin >> visitedLocation;
+    //cout << "Enter 1 for positive.\nEnter 2 for Negative.";
+    //cin >> var;
+    //if(var == 1)
+    //{
+    //    //update this patient is positive in the db
+    //    //update location database of new high risk location
 
-    }
-    else if(var == 2)
+    //}
+    //else if(var == 2)
+    //{
+    //    //update this patient is negative in the db
+    //}
+    //else
+    //{
+    //    cout << "Please select an option 1 or 2." << endl;
+    //}
+}
+
+
+
+/**
+* OPTION 1: Takes in new user info, stores info to DB, and also recommends if user needs a test
+* @param ...
+* @returns ...
+*
+* Remember:
+* - Check for illigal characters
+*       -> "Unknown selection, please try again"
+* - Alow for upper/lower characters
+*
+* (MW - 12/10/21)
+* - All DBs required
+* - For patient ID: scan through DB line by line only checking first column (ID)
+*       -> No DB = "[] - the database is empty"
+*       -> No match proceed / match "This ID exists"
+*       -> Order is not an issue atm
+* - If at High Risk Location with any symptoms, recomend test
+* - If High Risk Symptoms, recomend test
+* - Other symptoms -> "isolate at home" or something like that
+*       -> At least Try 5 more combination from real life scenario (???)
+* - If the Symptom database is empty you should display, "Unable to recommend COVID Test – required data missing".
+*
+*/
+void CovidTestRecommendationDetails()
+{
+    int     patientID;
+    cout << "Enter your patient ID:";
+    cin >> patientID; // if patientID already exists then cant send to db
+
+    ifstream ifileCheckPatientDB;
+    ifileCheckPatientDB.open("patientDB.txt");
+    if (ifileCheckPatientDB.fail())
     {
-        //update this patient is negative in the db
+        cout << "\nPatient Database Missing!\nPlease contact the application administrator on 555-123-456" << endl;
+        cout << "\nReturning to main menu.\n" << endl;
+        return;
     }
-    else
+
+    if (ifileCheckPatientDB.is_open())
     {
-        cout << "Please select an option 1 or 2." << endl;
+        string getRowLine,getRowVar;
+        vector<string> tempRow;
+        int compareID;
+
+        getline(ifileCheckPatientDB, getRowLine); // First row redundant - Might be a better method (replace this if so)
+        while (getline(ifileCheckPatientDB, getRowLine))
+        {
+            tempRow.clear(); // Memory Management - Clears last row saved
+
+            stringstream ss(getRowLine); // Splits Row into "Columns" (RowVar)
+
+            while (getline(ss,getRowVar,','))
+            {
+                tempRow.push_back(getRowVar);
+            }
+
+            compareID = stoi(tempRow[0]);
+
+            if (compareID == patientID)
+            {
+                cout << "match!" << endl;
+                break;
+            }
+            
+        }
     }
+    
+
+
+
+
+
+    //cout << "Enter your First name:";
+    //cin >> fname;
+
+    //cout << "Enter your Family name:";
+    //cin >> lname;
+
+    //cout << "Enter your Date of birth:";
+    //cin >> doB;
+
+    //cout << "Enter your address:";
+    //cin >> address;
+
+    //cout << "Have you travelled overseas in the last 6 months, Y/N?";
+    //cin >> overseasTravel;
+
+    //cout << "Please select your symptoms:"; // could just make an if/switch statement for this one with options 1-3. eg. 1: (Low risk) Dry cough. 2: (Medium risk) Fever
+    ////cin >> 
+
+    //cout << "Please select your visited location from the High Risk COVID areas:"; // similar to above, possibly display the db of high risk locations and display options 1-10 
+    //cin >> visitedLocation;
+
+    ////send variables to db
+
 }
 
 
@@ -288,12 +337,6 @@ void UpdateTestStatus()
 * @param ...
 * @returns ...
 *
-* Remember:
-* - Check for illigal characters
-*       -> "Unknown selection, please try again"
-* - If the Patient detail is empty you should display "[] - the database is empty"
-* - Alow for upper/lower characters
-*
 * (MW - 12/10/21)
 * This is mostly good; some minor additions/fixes will be needed
 * - I'll probably add some of my own flare eventually.
@@ -301,9 +344,7 @@ void UpdateTestStatus()
 */
 void DisplayMenu()
 {
-    int selection;
-
-    cin >> selection;
+    int selection(0);
 
     while(selection != 6)
     {
@@ -313,30 +354,36 @@ void DisplayMenu()
         cout << "4: Update COVID Patient Details" << endl;
         cout << "5: Display the COVID Positive patient details" << endl;
         cout << "6: Quit" << endl;
+        cout << "\n>> ";
+
         cin >> selection;
+
         if(!isalpha(selection))
         {
             switch (selection)
             {
                 case 1: 
                     CovidTestRecommendationDetails();
-                break;
+                    break;
 
                 case 2:
                     UpdateTestStatus();
-                break;
+                    break;
 
                 case 3:
                     DisplayUpdatedLocation();
-                break;
+                    break;
 
                 case 4:
                     UpdatePatientDetails();
-                break;
+                    break;
 
                 case 5:
                     DisplayPositivePatients();
-                break;
+                    break;
+
+                case 6:
+                    break;
 
                 default: 
                     cout << "Unknown selection, please try again" << endl;
@@ -344,7 +391,7 @@ void DisplayMenu()
         }
   
     } 
-    cout << "Goodbye";
+    cout << "Goodbye" << endl;
 }
 
 
@@ -357,7 +404,9 @@ void DisplayMenu()
 */
 int main () 
 { 
-    CreateDatabases();
+    //CreateDatabases();
+
+    cout << "\nWelcome\n" << endl;
     DisplayMenu();
     
     return 0; 
